dd46332b4d1a04f981c473921a96f3e8c3a5f6ab
Spencer Kersey <spencerkersey7702@gmail.com>
2024-09-08 16:19:47 -0400

    Fixed erroneous imports and naming conventions

--- tests.py
+++ tests.py
@@ -1,21 +1,20 @@
-import pytest
-import hw2_debugging
+from hw2_debugging import *
 
 # Tests the mergeSort function on an empty array
 def test_empty_array(): 
   arr = []
-  assert mergeSort(arr) == arr
+  assert merge_sort(arr) == arr
 
 # Tests the mergeSort function on an array with 1 value (odd number of values)
 def test_array_1():
   arr = [1]
-  assert mergeSort(arr) == arr
+  assert merge_sort(arr) == arr
 
 # Tests the mergeSort function on an array with 4 values (even number of values)
 def test_array_even():
   arr = [3, 1, 2, 4] 
   arr2 = [1,2,3,4]
-  assert mergeSort(arr) == arr2
+  assert merge_sort(arr) == arr2
   
   
   
