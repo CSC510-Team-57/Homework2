5dbd04d2f0312f2d5af8d75744cf000062825e07
Charlie-Eastin <charles98310@gmail.com>
2024-09-08 15:21:29 -0400

    Update tests.py with three test cases

--- tests.py
+++ tests.py
@@ -1,1 +1,19 @@
+import pytest
+import hw2_debugging
 
+def test_empty_array(): 
+  arr = []
+  assert mergeSort(arr) == arr
+
+def test_array_1():
+  arr = [1]
+  assert mergeSort(arr) == arr
+
+def test_array_even():
+  arr = [3, 1, 2, 4]
+  arr2 = [1,2,3,4]
+  assert mergeSort(arr) == arr2
+  
+  
+  
+    
