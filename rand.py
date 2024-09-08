dd26e3eb9a3a5da1ea87e86168bda1e555108516
Charlie-Eastin <charles98310@gmail.com>
2024-09-08 17:18:30 -0400

    Update rand.py

--- rand.py
+++ rand.py
@@ -8,9 +8,9 @@ import subprocess
 def random_array(arr):
     """ Shuffles an array randomly using its built in process """
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, element in enumerate(arr):
         shuffled_num = subprocess.run(
             ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
-        arr[i] = int(shuffled_num.stdout)
+        arr[i] = int(shuffled_num.stdout.strip()
     return arr
     
