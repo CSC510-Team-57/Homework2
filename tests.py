bee647bcc8f073c35bdc6caee84b63375701e002
Charlie-Eastin <charles98310@gmail.com>
2024-09-08 15:22:55 -0400

    Commented tests.py

--- tests.py
+++ tests.py
@@ -1,16 +1,19 @@
 import pytest
 import hw2_debugging
 
+# Tests the mergeSort function on an empty array
 def test_empty_array(): 
   arr = []
   assert mergeSort(arr) == arr
 
+# Tests the mergeSort function on an array with 1 value (odd number of values)
 def test_array_1():
   arr = [1]
   assert mergeSort(arr) == arr
 
+# Tests the mergeSort function on an array with 4 values (even number of values)
 def test_array_even():
-  arr = [3, 1, 2, 4]
+  arr = [3, 1, 2, 4] 
   arr2 = [1,2,3,4]
   assert mergeSort(arr) == arr2
   
