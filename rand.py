a86ff028bfc023c62d3d825f15843d46d40b23cb
Spencer Kersey <spencerkersey7702@gmail.com>
2024-09-08 17:05:24 -0400

    Attempt to fix error in enumerate function in rand.py

--- rand.py
+++ rand.py
@@ -8,8 +8,9 @@ import subprocess
 def random_array(arr):
     """ Shuffles an array randomly using its built in process """
     shuffled_num = None
-    for i, value in enumerate(arr):
+    for i in enumerate(arr):
         shuffled_num = subprocess.run(
             ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
-        value = int(shuffled_num.stdout)
+        i[1] = int(shuffled_num.stdout)
     return arr
+    
