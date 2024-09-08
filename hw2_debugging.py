fea9c184a0e5e014cf95ca2e6637d4c535c6c87f
Spencer Kersey <spencerkersey7702@gmail.com>
2024-09-08 16:07:36 -0400

    Added the two provided python files which Aastha has worked on

---
+++ hw2_debugging.py
@@ -0,0 +1,31 @@
+import rand
+
+
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+
+    half = len(arr) // 2
+    return recombine(merge_sort(arr[:half]), mergeSort(arr[half:]))
+
+
+def recombine(left_arr, right_arr):
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
+        else:
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
+
+    merge_arr[left_index + right_index:] = left_arr[left_index:] + right_arr[right_index:]
+    return merge_arr
+
+
+arr = rand.random_array([None] * 20)
+arr_out = merge_sort(arr)
+
+print(arr_out)
