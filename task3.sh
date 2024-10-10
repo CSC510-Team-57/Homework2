48bc58726a8b6943a4603dc3e4c2a2f14fddd527
Charles983 <97625324+Charles983@users.noreply.github.com>
2024-10-10 15:42:45 -0400

    "Fixed output of task3"

--- task3.sh
+++ task3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gawk -F, '$3 == 2 && $13 ~ /S/' titanic.csv
+gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/' titanic.csv | \
 
-cat titanic.csv | sed 's/female/F/g; s/male/M/g'
+sed 's/female/F/g; s/male/M/g' | \
 
-gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/ { age = $7; gsub(/^ *| *$/, "", age); if (age != "" ) { sum += age; count++ } } END { if (count > 0) { print "Average Age:", sum / count } else { print "No valid ages found." } }' titanic.csv
+gawk -F, '{ age = $7; gsub(/^ *| *$/, "", age); if (age != "" ) { sum += age; count++ } } END { if (count > 0) { print "Average Age:", sum / count } else { print "No valid ages found." } }'
 
